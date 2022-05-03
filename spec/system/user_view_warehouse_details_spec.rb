c1b3f784ed7cbec5271f2f3aa467c668feb21fc4
Marco Fornaciari <mfornaciari@gmail.com>
2022-05-03 19:01:36 -0300

    feat: cria link Voltar nos detalhes do galpão

--- spec/system/user_view_warehouse_details_spec.rb
+++ spec/system/user_view_warehouse_details_spec.rb
@@ -19,4 +19,19 @@ describe 'Usuário vê detalhes de um galpão' do
     expect(page).to have_content('Endereço: Avenida do Aeroporto, 1000 CEP: 15000-000')
     expect(page).to have_content('Galpão destinado para cargas internacionais.')
   end
+
+  it 'e volta para a tela inicial' do
+    # Arrange
+    Warehouse.create(name: 'Aeroporto SP', code: 'GRU', city: 'Guarulhos', area: 100_000,
+                     address: 'Avenida do Aeroporto, 1000', cep: '15000-000',
+                     description: 'Galpão destinado para cargas internacionais.')
+
+    # Act
+    visit('/')
+    click_on('Aeroporto SP')
+    click_on('Voltar')
+
+    # Assert
+    expect(current_path).to eq('/')
+  end
 end
