bb81049ace5b5b0e4b1b34c3b3225479bd300268
Marco Fornaciari <mfornaciari@gmail.com>
2022-05-08 10:38:37 -0300

    test: adiciona teste p/ dados inválidos Testes prévios não cobriam singularidade e formato de dados de cadastro

--- spec/system/user_register_warehouse_spec.rb
+++ spec/system/user_register_warehouse_spec.rb
@@ -45,4 +45,23 @@ describe 'Usuário cadastra um galpão' do
     # Assert
     fields.each { |field| expect(page).to have_content "#{field} não pode ficar em branco." }
   end
+
+  it 'com dados inválidos' do
+    # Arrange
+    Warehouse.create(name: 'Rio', description: 'Galpão do Rio', code: 'RIO', city: 'Rio de Janeiro',
+                     state: 'RJ', cep: '20000-000', area: 10_000)
+    fields_and_content = { 'Nome': 'Rio', 'Descrição': 'Galpão do Rio.', 'Código': 'RIO', 'Cidade': 'Rio de Janeiro',
+                           'Estado': 'RJ', 'CEP': '2', 'Área': '100000' }
+
+    # Act
+    visit root_path
+    click_on 'Cadastrar galpão'
+    fields_and_content.each_pair { |field, content| fill_in field, with: content }
+    click_on 'Criar Galpão'
+
+    # Assert
+    expect(page).to have_content 'Nome já está em uso.'
+    expect(page).to have_content 'Código já está em uso.'
+    expect(page).to have_content 'CEP não é válido.'
+  end
 end
