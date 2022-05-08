2c0d8c56aabdcdb3c69940aca867eaaeef164fc8
Marco Fornaciari <mfornaciari@gmail.com>
2022-05-08 18:47:14 -0300

    feat: implementa remoção de galpões Remoção feita a partir da tela de detalhes do galpão

--- app/controllers/warehouses_controller.rb
+++ app/controllers/warehouses_controller.rb
@@ -1,5 +1,5 @@
 class WarehousesController < ApplicationController
-  before_action :set_warehouse, only: %i[show edit update]
+  before_action :set_warehouse, only: %i[show edit update destroy]
 
   def new
     @warehouse = Warehouse.new
@@ -26,6 +26,11 @@ class WarehousesController < ApplicationController
       flash.now[:notice] = 'Galpão não atualizado.'
       render 'edit'
     end
+  end
+
+  def destroy
+    @warehouse.destroy
+    redirect_to root_path, notice: 'Galpão removido com sucesso.'
   end
 
   private
---
+++ spec/system/user_delete_warehouse_spec.rb
@@ -0,0 +1,24 @@
+require 'rails_helper'
+
+describe 'Usuário remove um galpão' do
+  it 'com sucesso' do
+    # Arrange
+    Warehouse.create!(name: 'Rio', description: 'Galpão da cidade do Rio.', code: 'SDU', city: 'Rio de Janeiro',
+                      state: 'RJ', cep: '20000-000', area: 60_000)
+    Warehouse.create!(name: 'Maceió', description: 'Galpão da cidade de Maceió.', code: 'MCZ', city: 'Maceió',
+                      state: 'AL', cep: '30000-000', area: 50_000)
+    rio_content = ['Rio', 'Código: SDU', 'Cidade: Rio de Janeiro', '60000 m2']
+    maceio_content = ['Maceió', 'Código: MCZ', 'Cidade: Maceió', '50000 m2']
+
+    # Act
+    visit root_path
+    click_on 'Rio'
+    click_on 'Remover'
+
+    # Assert
+    expect(current_path).to eq root_path
+    expect(page).to have_content 'Galpão removido com sucesso.'
+    rio_content.each { |content| expect(page).not_to have_content content }
+    maceio_content.each { |content| expect(page).to have_content content }
+  end
+end
