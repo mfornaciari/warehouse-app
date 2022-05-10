b37c35ce176950ea1c0456fc12fe6025f6981af3
Marco Fornaciari <mfornaciari@gmail.com>
2022-05-10 19:28:31 -0300

    feat: cria tela de cadastro de fornecedor Tela acessada a partir da tela de fornecedores

--- app/controllers/suppliers_controller.rb
+++ app/controllers/suppliers_controller.rb
@@ -7,9 +7,28 @@ class SuppliersController < ApplicationController
 
   def show; end
 
+  def new
+    @supplier = Supplier.new
+  end
+
+  def create
+    @supplier = Supplier.new supplier_params
+    if @supplier.save
+      redirect_to suppliers_path, notice: 'Fornecedor cadastrado com sucesso.'
+    else
+      flash.now[:notice] = 'Fornecedor não cadastrado.'
+      render 'new'
+    end
+  end
+
   private
 
   def set_supplier
     @supplier = Supplier.find params[:id]
+  end
+
+  def supplier_params
+    params.require(:supplier).permit(:brand_name, :corporate_name, :registration_number,
+                                     :address, :city, :state, :cep, :email, :phone)
   end
 end
---
+++ spec/system/suppliers/user_register_supplier_spec.rb
@@ -0,0 +1,91 @@
+require 'rails_helper'
+
+describe 'Usuário cadastra um fornecedor' do
+  it 'a partir da tela inicial' do
+    # Arrange
+    fields = ['Nome fantasia', 'Razão social', 'CNPJ', 'Endereço',
+              'Cidade', 'Estado', 'CEP', 'E-mail', 'Telefone']
+
+    # Act
+    visit root_path
+    click_on 'Fornecedores'
+    click_on 'Cadastrar fornecedor'
+
+    # Assert
+    fields.each { |field| expect(page).to have_field field }
+  end
+
+  it 'com sucesso' do
+    # Arrange
+    fields_and_content = { 'Nome fantasia': 'ACME', 'Razão social': 'ACME Ltda.', 'CNPJ': '1234567890123',
+                           'E-mail': 'contato@acme.com' }
+
+    # Act
+    visit root_path
+    click_on 'Fornecedores'
+    click_on 'Cadastrar fornecedor'
+    fields_and_content.each_pair { |field, content| fill_in field, with: content }
+    click_on 'Criar Fornecedor'
+
+    # Assert
+    expect(current_path).to eq suppliers_path
+    expect(page).to have_content 'Fornecedor cadastrado com sucesso.'
+    expect(page).to have_content 'ACME'
+  end
+
+  it 'com dados incompletos' do
+    # Arrange
+    blank_fields = ['Nome fantasia', 'Razão social', 'CNPJ', 'E-mail']
+    filled_fields = { 'Endereço': 'Avenida A, 10', 'Cidade': 'Natal', 'Estado': 'RN', 'CEP': '12345-678',
+                      'Telefone': '91234-5678' }
+
+    # Act
+    visit root_path
+    click_on 'Fornecedores'
+    click_on 'Cadastrar fornecedor'
+    filled_fields.each_pair { |field, content| fill_in field, with: content }
+    click_on 'Criar Fornecedor'
+
+    # Assert
+    blank_fields.each { |field| expect(page).to have_content "#{field} não pode ficar em branco." }
+    filled_fields.each_pair { |field, content| expect(page). to have_field field, with: content }
+  end
+
+  it 'com CNPJ repetido' do
+    # Arrange
+    Supplier.create!(brand_name: 'ACME', corporate_name: 'ACME Ltda.', registration_number: 1_234_567_890_123,
+                     email: 'contato@acme.com')
+    repeated_number_supplier = { 'Nome fantasia': 'Stark', 'Razão social': 'Stark Industries Brasil Ltda.',
+                                 'CNPJ': '1234567890123', 'E-mail': 'contato@stark.com.br' }
+    invalid_number_supplier = { 'Nome fantasia': 'Campus', 'Razão social': 'Campus S/A',
+                                'CNPJ': '123', 'E-mail': 'contato@campus.com' }
+
+    # Act
+    visit root_path
+    click_on 'Fornecedores'
+    click_on 'Cadastrar fornecedor'
+    repeated_number_supplier.each_pair { |field, content| fill_in field, with: content }
+    click_on 'Criar Fornecedor'
+
+    # Assert
+    expect(page).to have_content 'CNPJ já está em uso.'
+  end
+
+  it 'com CNPJ inválido' do
+    # Arrange
+    Supplier.create!(brand_name: 'ACME', corporate_name: 'ACME Ltda.', registration_number: 1_234_567_890_123,
+                     email: 'contato@acme.com')
+    invalid_number_supplier = { 'Nome fantasia': 'Campus', 'Razão social': 'Campus S/A',
+                                'CNPJ': '123', 'E-mail': 'contato@campus.com' }
+
+    # Act
+    visit root_path
+    click_on 'Fornecedores'
+    click_on 'Cadastrar fornecedor'
+    invalid_number_supplier.each_pair { |field, content| fill_in field, with: content }
+    click_on 'Criar Fornecedor'
+
+    # Assert
+    expect(page).to have_content 'CNPJ não é válido.'
+  end
+end
