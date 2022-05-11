e84bef73b1c4ec8ad60dc19e479ad3f07db62cf1
Marco Fornaciari <mfornaciari@gmail.com>
2022-05-11 20:11:09 -0300

    feat: implementa validação p/ modelo de produto

--- app/models/product_model.rb
+++ app/models/product_model.rb
@@ -1,3 +1,7 @@
 class ProductModel < ApplicationRecord
   belongs_to :supplier
+  validates :name, :weight, :height, :width, :depth, :code, presence: true
+  validates :code, uniqueness: true
+  validates :code, format: { with: /\A\S{20}\z/ }
+  validates :weight, :height, :width, :depth, comparison: { greater_than: 0 }
 end
---
+++ spec/models/product_model_spec.rb
@@ -0,0 +1,204 @@
+require 'rails_helper'
+
+RSpec.describe ProductModel, type: :model do
+  describe '#valid?' do
+    context 'presença' do
+      it 'falso quando nome está vazio' do
+        # Arrange
+        acme = Supplier.create!(brand_name: 'ACME', corporate_name: 'ACME Ltda.',
+                                registration_number: 1_234_567_890_123, address: '', city: '', state: '', cep: '',
+                                email: 'contato@acme.com', phone: '')
+        product_model = ProductModel.new(name: '', weight: 2_000, height: 15, width: 25, depth: 10,
+                                         code: 'NOTE-MARCA-123456789', supplier: acme)
+
+        # Act
+
+        # Assert
+        expect(product_model).not_to be_valid
+      end
+
+      it 'falso quando peso está vazio' do
+        # Arrange
+        acme = Supplier.create!(brand_name: 'ACME', corporate_name: 'ACME Ltda.',
+                                registration_number: 1_234_567_890_123, address: '', city: '', state: '', cep: '',
+                                email: 'contato@acme.com', phone: '')
+        product_model = ProductModel.new(name: 'Notebook', weight: '', height: 15, width: 25, depth: 10,
+                                         code: 'NOTE-MARCA-123456789', supplier: acme)
+
+        # Act
+
+        # Assert
+        expect(product_model).not_to be_valid
+      end
+
+      it 'falso quando altura está vazia' do
+        # Arrange
+        acme = Supplier.create!(brand_name: 'ACME', corporate_name: 'ACME Ltda.',
+                                registration_number: 1_234_567_890_123, address: '', city: '', state: '', cep: '',
+                                email: 'contato@acme.com', phone: '')
+        product_model = ProductModel.new(name: 'Notebook', weight: 2_000, height: '', width: 25, depth: 10,
+                                         code: 'NOTE-MARCA-123456789', supplier: acme)
+
+        # Act
+
+        # Assert
+        expect(product_model).not_to be_valid
+      end
+
+      it 'falso quando largura está vazia' do
+        # Arrange
+        acme = Supplier.create!(brand_name: 'ACME', corporate_name: 'ACME Ltda.',
+                                registration_number: 1_234_567_890_123, address: '', city: '', state: '', cep: '',
+                                email: 'contato@acme.com', phone: '')
+        product_model = ProductModel.new(name: 'Notebook', weight: 2_000, height: 15, width: '', depth: 10,
+                                         code: 'NOTE-MARCA-123456789', supplier: acme)
+
+        # Act
+
+        # Assert
+        expect(product_model).not_to be_valid
+      end
+
+      it 'falso quando profundidade está vazia' do
+        # Arrange
+        acme = Supplier.create!(brand_name: 'ACME', corporate_name: 'ACME Ltda.',
+                                registration_number: 1_234_567_890_123, address: '', city: '', state: '', cep: '',
+                                email: 'contato@acme.com', phone: '')
+        product_model = ProductModel.new(name: 'Notebook', weight: 2_000, height: 15, width: 25, depth: '',
+                                         code: 'NOTE-MARCA-123456789', supplier: acme)
+
+        # Act
+
+        # Assert
+        expect(product_model).not_to be_valid
+      end
+
+      it 'falso quando código está vazio' do
+        # Arrange
+        acme = Supplier.create!(brand_name: 'ACME', corporate_name: 'ACME Ltda.',
+                                registration_number: 1_234_567_890_123, address: '', city: '', state: '', cep: '',
+                                email: 'contato@acme.com', phone: '')
+        product_model = ProductModel.new(name: 'Notebook', weight: 2_000, height: 15, width: 25, depth: 10,
+                                         code: '', supplier: acme)
+
+        # Act
+
+        # Assert
+        expect(product_model).not_to be_valid
+      end
+    end
+
+    context 'singularidade' do
+      it 'falso quando código já foi cadastrado' do
+        # Arrange
+        acme = Supplier.create!(brand_name: 'ACME', corporate_name: 'ACME Ltda.',
+                                registration_number: 1_234_567_890_123, address: '', city: '', state: '', cep: '',
+                                email: 'contato@acme.com', phone: '')
+        ProductModel.create!(name: 'Notebook', weight: 2_000, height: 15, width: 25, depth: 10,
+                             code: 'NOTE-MARCA-123456789', supplier: acme)
+        tablet = ProductModel.new(name: 'Tablet', weight: 1_000, height: 10, width: 8, depth: 1,
+                                  code: 'NOTE-MARCA-123456789', supplier: acme)
+
+        # Act
+
+        # Assert
+        expect(tablet).not_to be_valid
+      end
+    end
+
+    context 'formato' do
+      it 'falso quando formato do código é incorreto' do
+        # Arrange
+        acme = Supplier.create!(brand_name: 'ACME', corporate_name: 'ACME Ltda.',
+                                registration_number: 1_234_567_890_123, address: '', city: '', state: '', cep: '',
+                                email: 'contato@acme.com', phone: '')
+        invalid_models = []
+        invalid_models << ProductModel.new(name: 'Notebook', weight: 2_000, height: 15, width: 25, depth: 10,
+                                           code: 'A', supplier: acme)
+        invalid_models << ProductModel.new(name: 'Notebook', weight: 2_000, height: 15, width: 25, depth: 10,
+                                           code: '123456789012345678901234567890', supplier: acme)
+        invalid_models << ProductModel.new(name: 'Notebook', weight: 2_000, height: 15, width: 25, depth: 10,
+                                           code: '1 2 3 4 5 6 7 8 9 0 ', supplier: acme)
+
+        valid_model = ProductModel.new(name: 'Tablet', weight: 1_000, height: 10, width: 8, depth: 1,
+                                       code: 'NOTE-MARCA-123456789', supplier: acme)
+
+        # Act
+
+        # Assert
+        invalid_models.each { |model| expect(model).not_to be_valid }
+        expect(valid_model).to be_valid
+      end
+    end
+
+    context 'valor' do
+      it 'falso quando peso <= 0' do
+        # Arrange
+        acme = Supplier.create!(brand_name: 'ACME', corporate_name: 'ACME Ltda.',
+                                registration_number: 1_234_567_890_123, address: '', city: '', state: '', cep: '',
+                                email: 'contato@acme.com', phone: '')
+        notebook = ProductModel.new(name: 'Notebook', weight: 0, height: 15, width: 25, depth: 10,
+                                    code: 'NOTE-MARCA-123456789', supplier: acme)
+        tablet = ProductModel.new(name: 'Tablet', weight: -1, height: 10, width: 8, depth: 1,
+                                  code: 'NOTE-MARCA-123456789', supplier: acme)
+
+        # Act
+
+        # Assert
+        expect(notebook).not_to be_valid
+        expect(tablet).not_to be_valid
+      end
+
+      it 'falso quando altura <= 0' do
+        # Arrange
+        acme = Supplier.create!(brand_name: 'ACME', corporate_name: 'ACME Ltda.',
+                                registration_number: 1_234_567_890_123, address: '', city: '', state: '', cep: '',
+                                email: 'contato@acme.com', phone: '')
+        notebook = ProductModel.new(name: 'Notebook', weight: 2_000, height: 0, width: 25, depth: 10,
+                                    code: 'NOTE-MARCA-123456789', supplier: acme)
+        tablet = ProductModel.new(name: 'Tablet', weight: 500, height: -1, width: 8, depth: 1,
+                                  code: 'NOTE-MARCA-123456789', supplier: acme)
+
+        # Act
+
+        # Assert
+        expect(notebook).not_to be_valid
+        expect(tablet).not_to be_valid
+      end
+
+      it 'falso quando largura <= 0' do
+        # Arrange
+        acme = Supplier.create!(brand_name: 'ACME', corporate_name: 'ACME Ltda.',
+                                registration_number: 1_234_567_890_123, address: '', city: '', state: '', cep: '',
+                                email: 'contato@acme.com', phone: '')
+        notebook = ProductModel.new(name: 'Notebook', weight: 2_000, height: 15, width: 0, depth: 10,
+                                    code: 'NOTE-MARCA-123456789', supplier: acme)
+        tablet = ProductModel.new(name: 'Tablet', weight: 500, height: 10, width: -1, depth: 1,
+                                  code: 'NOTE-MARCA-123456789', supplier: acme)
+
+        # Act
+
+        # Assert
+        expect(notebook).not_to be_valid
+        expect(tablet).not_to be_valid
+      end
+
+      it 'falso quando profundidade <= 0' do
+        # Arrange
+        acme = Supplier.create!(brand_name: 'ACME', corporate_name: 'ACME Ltda.',
+                                registration_number: 1_234_567_890_123, address: '', city: '', state: '', cep: '',
+                                email: 'contato@acme.com', phone: '')
+        notebook = ProductModel.new(name: 'Notebook', weight: 2_000, height: 15, width: 25, depth: 0,
+                                    code: 'NOTE-MARCA-123456789', supplier: acme)
+        tablet = ProductModel.new(name: 'Tablet', weight: 500, height: 10, width: 8, depth: -1,
+                                  code: 'NOTE-MARCA-123456789', supplier: acme)
+
+        # Act
+
+        # Assert
+        expect(notebook).not_to be_valid
+        expect(tablet).not_to be_valid
+      end
+    end
+  end
+end
