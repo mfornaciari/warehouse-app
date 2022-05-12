9f95ef98e2e998dd3ae51d8200aa3323253cba6e
Marco Fornaciari <mfornaciari@gmail.com>
2022-05-12 07:39:43 -0300

    feat: cria tela de detalhes do modelo de produto Tela acessada a partir da lista de modelos de produto

--- app/controllers/product_models_controller.rb
+++ app/controllers/product_models_controller.rb
@@ -1,5 +1,15 @@
 class ProductModelsController < ApplicationController
+  before_action :set_product_model, only: %i[show]
+
   def index
     @product_models = ProductModel.all
+  end
+
+  def show; end
+
+  private
+
+  def set_product_model
+    @product_model = ProductModel.find params[:id]
   end
 end
--- config/routes.rb
+++ config/routes.rb
@@ -2,7 +2,7 @@ Rails.application.routes.draw do
   # Define your application routes per the DSL in https://guides.rubyonrails.org/routing.html
   resources :warehouses, only: %i[new create show edit update destroy]
   resources :suppliers, only: %i[index show new create edit update]
-  resources :product_models, only: %i[index]
+  resources :product_models, only: %i[index show]
   # Defines the root path route ("/")
   root 'home#index'
 end
