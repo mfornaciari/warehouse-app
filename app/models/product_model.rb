cd997feb7ad5d2835e1895c48b8d8ee1c881a379
Marco Fornaciari <mfornaciari@gmail.com>
2022-05-13 10:42:20 -0300

    refactor: remove validação desnecessária Validação de valor já valida existência do atributo

--- app/models/product_model.rb
+++ app/models/product_model.rb
@@ -1,6 +1,6 @@
 class ProductModel < ApplicationRecord
   belongs_to :supplier
-  validates :name, :weight, :height, :width, :depth, :code, presence: true
+  validates :name, :code, presence: true
   validates :code, uniqueness: true
   validates :code, format: { with: /\A\S{20}\z/ }
   validates :weight, :height, :width, :depth, comparison: { greater_than: 0 }
